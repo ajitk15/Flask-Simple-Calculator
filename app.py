d8d05c508e3adf63079fc2fab07e31cb4f0d5138
ajitk15 <ajitk15@gmail.com>
2023-09-18 22:44:05 +0530

    second commit

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ app = Flask(__name__)
 def hello_world():
     return "<h1>Hello World!</h1>"
 
+@app.route("/hello1")
+def hello_world1():
+    return "<h1>Hello World1111!</h1>"
 
 if __name__ =="__main__":
     app.run(host="0.0.0.0")
