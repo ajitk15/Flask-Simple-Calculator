fa25383f3d8a5c2bfa7d2d94d0200f5b7b34f3c8
ajitk15 <ajitk15@gmail.com>
2023-09-19 00:05:49 +0530

    Flask Simple Calculator

--- app.py
+++ app.py
@@ -1,14 +1,65 @@
-from flask import Flask
+from flask import Flask, render_template, request, jsonify
 
-app = Flask(__name__)
+app = Flask("__name__")
 
 @app.route("/")
-def hello_world():
-    return "<h1>Hello World!</h1>"
+def index():
+    return render_template("index.html")
 
-@app.route("/hello1")
-def hello_world1():
-    return "<h1>Hello World1111!</h1>"
 
-if __name__ =="__main__":
-    app.run(host="0.0.0.0")
+@app.route("/math", methods=['POST'])
+def math():
+    if(request.method == 'POST'):
+        ops = request.form['operation']
+        num1 = int(request.form['num1'])
+        num2 = int(request.form['num2'])
+        if ops == 'add':
+            r = num1 + num2
+            result = "The sum of " + str(num1) + ' and ' + str(num2) + " is " + str(r)
+        if ops == 'subtract':
+            r = num1 - num2
+            result = "The subtract of " + str(num1) + ' and ' + str(num2) + " is " + str(r)
+        if ops == 'multiply':
+            r = num1 * num2
+            result = "The multiply of " + str(num1) + ' and ' + str(num2) + " is " + str(r)
+        if ops == 'divide':
+            r = num1 / num2
+            result = "The divide of " + str(num1) + ' and ' + str(num2) + " is " + str(r)
+        
+        return render_template('results.html', result=result)
+
+
+
+"""
+curl --location 'https://lemon-author-lfpbg.pwskills.app:5000/webapimath' \
+--header 'Content-Type: application/json' \
+--data '{
+    "operation":"add",
+    "num1":100,
+    "num2":20
+}'
+"""
+@app.route("/webapimath", methods=['POST'])
+def math_webapi():
+    if(request.method == 'POST'):
+        ops = request.json['operation']
+        num1 = int(request.json['num1'])
+        num2 = int(request.json['num2'])
+        if ops == 'add':
+            r = num1 + num2
+            result = "The sum of " + str(num1) + ' and ' + str(num2) + " is " + str(r)
+        if ops == 'subtract':
+            r = num1 - num2
+            result = "The subtract of " + str(num1) + ' and ' + str(num2) + " is " + str(r)
+        if ops == 'multiply':
+            r = num1 * num2
+            result = "The multiply of " + str(num1) + ' and ' + str(num2) + " is " + str(r)
+        if ops == 'divide':
+            r = num1 / num2
+            result = "The divide of " + str(num1) + ' and ' + str(num2) + " is " + str(r)
+        
+        return jsonify(result)
+
+
+if __name__ == "__main__":
+    app.run("0.0.0.0")
